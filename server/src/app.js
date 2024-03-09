c3a184ca2f194622bf4c5c5deec3349d675511fd
jesusre890 <jesusre890@gmail.com>
2024-03-08 22:47:06 -0300

    cambio back 2

--- server/src/app.js
+++ server/src/app.js
@@ -12,10 +12,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://challenge-products-sd.vercel.app/"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
